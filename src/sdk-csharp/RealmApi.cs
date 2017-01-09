ec7864ac4a5bd0bc00b8fd8b2d53361d5951369f
Eric Mann <eric@tozny.com>
2017-01-09 11:20:00 -0800

    Properly add users

--- src/sdk-csharp/RealmApi.cs
+++ src/sdk-csharp/RealmApi.cs
@@ -28,7 +28,61 @@ namespace Tozny.Auth
 			this.realmSecret = realmSecret;
 		}
 
-		public async Task user_add(Boolean defer, Dictionary<String, String> fields, String public_key)
+		public class UserAddResponse
+		{
+			protected String user_id;
+			protected String user_key_id;
+			protected String temp_user_secret;
+			protected String secret_enrollment_url;
+			protected String secret_enrollment_qr_url;
+			protected String realm_id;
+			protected String realm_key_id;
+			protected String logo_url;
+			protected String info_url;
+			protected String crypto_suite;
+			protected String display_name;
+
+			public UserAddResponse(
+				String user_id,
+				String user_key_id,
+				String temp_user_secret,
+				String secret_enrollment_url,
+				String secret_enrollment_qr_url,
+				String realm_id,
+				String realm_key_id,
+				String logo_url,
+				String info_url,
+				String crypto_suite,
+				String display_name
+				)
+			{
+				this.user_id = user_id;
+				this.user_key_id = user_key_id;
+				this.temp_user_secret = temp_user_secret;
+				this.secret_enrollment_url = secret_enrollment_url;
+				this.secret_enrollment_qr_url = secret_enrollment_qr_url;
+				this.realm_id = realm_id;
+				this.realm_key_id = realm_key_id;
+				this.logo_url = logo_url;
+				this.info_url = info_url;
+				this.crypto_suite = crypto_suite;
+				this.display_name = display_name;
+			}
+
+			public String UserID { get { return this.user_id; } }
+			public String UserKeyId { get { return this.user_key_id; } }
+			public String TempUserSecret { get { return this.temp_user_secret; } }
+			public String SecretEnrollmentUrl { get { return this.secret_enrollment_url; } }
+			public String SecretEnrollmentQrUrl { get { return this.secret_enrollment_qr_url; } }
+			public String RealmId { get { return this.realm_id; } }
+			public String RealmKeyId { get { return this.realm_key_id; } }
+			public String LogoUrl { get { return this.logo_url; } }
+			public String InfoUrl { get { return this.info_url; } }
+			public String CryptoSuite { get { return this.crypto_suite; } }
+			public String DisplayName { get { return this.display_name; } }
+		}
+		
+		public async Task<UserAddResponse> user_add(Boolean defer, Dictionary<String, String> fields, String public_key)
 		{
 			var request = new RealmRequest("user_add");
 			request.Add("defer", defer ? "true" : "false");
@@ -41,17 +95,38 @@ namespace Tozny.Auth
 				request.Add("pub_key", public_key);
 			}
 
-			try
+			dynamic response = await this.rawCall(request);
+
+			if (response.GetValue("return") == "ok")
 			{
-				dynamic response = await this.rawCall(request);
+				String user_id = response.GetValue("user_id");
+				String user_key_id = response.GetValue("user_key_id");
+				String temp_user_secret = response.GetValue("temp_user_secret");
+				String secret_enrollment_url = response.GetValue("secret_enrollment_url");
+				String secret_enrollment_qr_url = response.GetValue("secret_enrollment_qr_url");
+				String realm_id = response.GetValue("realm_id");
+				String realm_key_id = response.GetValue("realm_key_id");
+				String logo_url = response.GetValue("logo_url");
+				String info_url = response.GetValue("info_url");
+				String crypto_suite = response.GetValue("crypto_suite");
+				String display_name = response.GetValue("display_name");
 
-				if (response.GetValue("return") == "ok")
-				{
-
-				}
-			} catch(Exception e)
+				return new UserAddResponse(
+					user_id,
+					user_key_id,
+					temp_user_secret,
+					secret_enrollment_url,
+					secret_enrollment_qr_url,
+					realm_id,
+					realm_key_id,
+					logo_url,
+					info_url,
+					crypto_suite,
+					display_name
+					);
+			} else
 			{
-
+				throw new Exception("API Error");
 			}
 		}
 
