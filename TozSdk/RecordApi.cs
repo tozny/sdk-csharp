f0ccaf8df17086d09277513c124875e148be96c4
Lily Tsou <lily@tozny.com>
2024-05-08 09:59:40 -0700

    condense code

--- TozSdk/RecordApi.cs
+++ TozSdk/RecordApi.cs
@@ -7,19 +7,14 @@ namespace Tozny.Auth
     {
         public string DecryptRecord(byte[] accessKey, string encryptedData)
         {
-            string[] dataFields = encryptedData.Split('.');
-            string edk = dataFields[0];
-            string edkN = dataFields[1];
-            string ef = dataFields[2];
-            string efN = dataFields[3];
-
+            string[] dottedQuad = encryptedData.Split('.');
             // Decode data key
-            byte[] edkBytes = Base64UrlDecode(edk);
-            byte[] edkNBytes = Base64UrlDecode(edkN);
+            byte[] edkBytes = Base64UrlDecode(dottedQuad[0]);
+            byte[] edkNBytes = Base64UrlDecode(dottedQuad[1]);
             var dataKey = SecretBox.Open(edkBytes, edkNBytes, accessKey);
             // Decode data
-            byte[] efBytes = Base64UrlDecode(ef);
-            byte[] efNBytes = Base64UrlDecode(efN);
+            byte[] efBytes = Base64UrlDecode(dottedQuad[2]);
+            byte[] efNBytes = Base64UrlDecode(dottedQuad[3]);
             var data = SecretBox.Open(efBytes, efNBytes, dataKey);
             var plainText = System.Text.Encoding.Default.GetString(data);
             return plainText;
